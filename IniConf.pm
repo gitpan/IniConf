e8972a094d92c10e0736306cfe5c4696ba17d5d6
Rich Bowen <rbowen@rcbowen.com>
2000-08-05 01:37:11 +0000

    Import of RBOW/IniConf-1.01 from CPAN.
    gitpan-cpan-distribution: IniConf
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         RBOW/IniConf-1.01.tar.gz
    gitpan-cpan-author:       RBOW
    gitpan-cpan-maturity:     released

--- IniConf.pm
+++ IniConf.pm
@@ -1,5 +1,20 @@
 package IniConf;
-$VERSION = '1.00';
-use Config::IniFiles;
-1;
 
+use strict;
+use Config::IniFiles;
+
+use vars       qw($VERSION @ISA);
+$VERSION = '1.01';
+@ISA         = qw(Config::IniFiles);
+
+sub new {
+    my $self = new Config::IniFiles( @_ );
+    return undef unless defined $self;
+
+    my $proto = shift;
+    my $class = ref($proto) || $proto;
+    bless ($self, $class);
+
+    return $self;
+}
+1;
